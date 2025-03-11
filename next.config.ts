1db7cbad54e2c6cd6d2fcb156b0532a4de7fdbdc
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-11 18:38:22 +0700

    ignore nock

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.alias["nock"] = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
