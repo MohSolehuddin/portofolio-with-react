d8806a1750ae92ab101f20f30c57f62ae935252b
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-11 18:45:59 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.alias["nock"] = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
