d8fef983da2b2980a54c1d09bee797a8f0f24f9d
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-11 17:39:07 +0700

    FIX: delete depecreted method

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
-  target: "serverless",
 };
 
 export default nextConfig;
