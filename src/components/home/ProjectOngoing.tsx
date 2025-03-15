2656b4b6d001fea4d67d7f60b865d11c5aedf560
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-15 21:52:07 +0700

    FEAT: get ongoing project

--- src/components/home/ProjectOngoing.tsx
+++ src/components/home/ProjectOngoing.tsx
@@ -1,5 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import ProgressStat from "../stats/ProgressStat";
 
 export default function ProjectOngoing() {
-  return <ProgressStat value={0} text="Project Ongoing" />;
+  const [ongoingProjects, setOngoingProjects] = useState(0);
+
+  useEffect(() => {
+    const getTotalOngoingProjects = async () => {
+      try {
+        const response = await axios.get("/api/v1/projects/ongoing");
+        setOngoingProjects(response.data.data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+    getTotalOngoingProjects();
+  });
+  return <ProgressStat value={ongoingProjects} text="Project Ongoing" />;
 }
