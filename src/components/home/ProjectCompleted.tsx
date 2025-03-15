89d3af9dd706241d339c40a7d5b6956dd3402a91
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-15 21:54:10 +0700

    FEAT: get total complete  project

--- src/components/home/ProjectCompleted.tsx
+++ src/components/home/ProjectCompleted.tsx
@@ -1,5 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import ProgressStat from "../stats/ProgressStat";
 
 export default function ProjectCompleted() {
-  return <ProgressStat value={0} text="Project Completed" />;
+  const [completedProjects, setCompletedProjects] = useState(0);
+
+  useEffect(() => {
+    const completedProjects = async () => {
+      try {
+        const response = await axios.get("/api/v1/projects/completed");
+        setCompletedProjects(response.data.data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+    completedProjects();
+  });
+  return <ProgressStat value={completedProjects} text="Project Completed" />;
 }
