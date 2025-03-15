434d23d160724f1139dbf4063f8a12a7f65550f7
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-15 21:29:40 +0700

    FEAT: add endpoint to get counter ongoing project

---
+++ src/app/api/v1/projects/ongoing/route.ts
@@ -0,0 +1,10 @@
+import { db } from "@/server/db";
+
+export async function GET() {
+  const countCompletedProjects = await db.portfolio.count({
+    where: {
+      completedAt: null,
+    },
+  });
+  return Response.json({ data: countCompletedProjects }, { status: 200 });
+}
