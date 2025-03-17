7ceb4248f2e469f8329cffafa1f9f8b19399cc8a
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-17 12:38:56 +0700

    FEAT: get products

---
+++ src/app/axios/features/project.ts
@@ -0,0 +1,17 @@
+import PagingInterface from "@/lib/interfaces/PagingInterfaces";
+import axios from "axios";
+import axiosInstance from "../axiosInstance";
+
+export const getProjects = async ({ page, limit }: PagingInterface) => {
+  try {
+    const response = await axiosInstance.get(
+      `/projects?page=${page}&limit=${limit}`
+    );
+    return response.data.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.log(error);
+    }
+    throw error;
+  }
+};
