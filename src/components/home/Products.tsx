9e3c591f45d0559bacb892c6a91171aba6849993
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-18 16:10:38 +0700

    FIX: positioning error and loading display

--- src/components/home/Products.tsx
+++ src/components/home/Products.tsx
@@ -27,19 +27,31 @@ function ProductsContent() {
     queryKey: ["projects", page],
     queryFn: () => getProjects({ page, limit: 3 }),
   });
-  console.log(data);
 
-  if (isLoading) return <Loading />;
+  if (isLoading)
+    return (
+      <section className="h-screen flex justify-center items-center">
+        <Loading />
+      </section>
+    );
   if (error)
-    return <p className="text-center text-red-500">Error loading projects.</p>;
+    return (
+      <section className="h-screen flex justify-center items-center">
+        <p className="text-center text-red-500">Error loading projects.</p>
+      </section>
+    );
 
   const validatedProjects = PortfolioInputSchema.array().safeParse(data.data);
   if (!validatedProjects.success)
-    return <p className="text-center text-red-500">Invalid data format.</p>;
+    return (
+      <section className="h-screen flex justify-center items-center">
+        <p className="text-center text-red-500">Invalid data format.</p>
+      </section>
+    );
 
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center">
-      <h3 className="text-5xl text-navy dark:text-light mb-12 font-extrabold text-center">
+    <section className="min-h-screen flex flex-col justify-center items-center gap-12">
+      <h3 className="text-5xl text-navy dark:text-light font-extrabold text-center">
         My Recent Projects
       </h3>
       <section className="z-40 w-full flex flex-wrap justify-center items-center gap-6">
