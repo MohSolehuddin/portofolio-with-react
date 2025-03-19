2ef0e610409ed85d86845e97819e80bca35c1931
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-19 12:40:52 +0700

    FEAT: add crud for logging error

---
+++ src/data/errorLog.ts
@@ -0,0 +1,34 @@
+import { db } from "@/server/db";
+
+export const createErrorLog = async (error: string) => {
+  try {
+    return await db.errorLog.create({
+      data: {
+        message: error,
+      },
+    });
+  } catch (err) {
+    const errorLogInString = JSON.stringify(err);
+    await createErrorLog(errorLogInString);
+  }
+};
+
+export const getAllErrorLog = async () => {
+  return db.errorLog.findMany();
+};
+
+export const deleteErrorLog = async (id: string) => {
+  return db.errorLog.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export const getErrorLogById = async (id: string) => {
+  return db.errorLog.findUnique({
+    where: {
+      id,
+    },
+  });
+};
