e5fd43b5d622648ff0d9763ad7675f3b793d2d87
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-19 13:53:36 +0700

    FEAT: add SocialMedia

---
+++ src/components/socialMedia/SocialMedia.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { FaGithub, FaInstagram, FaTiktok } from "react-icons/fa";
+export default function SocialMedia() {
+  return (
+    <section className="social-media flex justify-center items-center gap-6 text-3xl">
+      <Link href="https://github.com/MohSolehuddin" target="_blank">
+        <FaGithub />
+      </Link>
+      <Link href="https://www.tiktok.com/@msytcode" target="_blank">
+        <FaTiktok />
+      </Link>
+      <Link href="https://www.instagram.com/msytc" target="_blank">
+        <FaInstagram />
+      </Link>
+    </section>
+  );
+}
