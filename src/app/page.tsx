c01de9d8636db11130e33ce99bf3a6d6e29ba7a4
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-21 14:50:55 +0700

    FEAT: change name from product to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import ContainerHome from "@/components/home/ContainerHome";
 import HeroSection from "@/components/home/HeroSection";
 import MySkill from "@/components/home/MySkill";
-import Products from "@/components/home/Products";
+import Products from "@/components/home/Projects";
 
 export default function Page() {
   return (
