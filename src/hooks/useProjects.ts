ddecc22651a7c72eefd532fc48f34a1a03781754
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-21 11:18:52 +0700

    FEAT: create custom hooks for project

---
+++ src/hooks/useProjects.ts
@@ -0,0 +1,37 @@
+"use client";
+import { getProjects } from "@/app/axios/features/project";
+import { PortfolioInputSchema } from "@/lib/schema/portfolioSchema";
+import { useQuery } from "@tanstack/react-query";
+import { z } from "zod";
+
+export interface useProjectsInterface {
+  projects: z.infer<typeof PortfolioInputSchema>[];
+  paging: { totalPage: number };
+  isLoading: boolean;
+  error: unknown;
+}
+
+export default function useProjects({
+  page,
+  limit,
+}: {
+  page: number;
+  limit: number;
+}) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["projects", page],
+    queryFn: () => getProjects({ page, limit }),
+  });
+
+  const validatedProjects = data
+    ? PortfolioInputSchema.array().safeParse(data.data)
+    : { success: false, data: [] };
+
+  return {
+    projects: validatedProjects.data || [],
+    paging: data ? data.paging : { totalPage: 0 },
+    isLoading,
+    error,
+    isValidatedProjectsSuccess: validatedProjects.success,
+  };
+}
