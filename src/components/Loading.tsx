ecfd963a28bfc00d03debfe176c0024cc52a9da7
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-24 10:50:21 +0700

    FEAT: make easy to use and simplify using loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,13 @@
-export default function Loading() {
+import clsx from "clsx";
+
+export default function Loading({ className }: { className?: string }) {
   return (
-    <div className="flex flex-row gap-2">
-      <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:.5s]"></div>
-      <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:.3s]"></div>
-      <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:.1s]"></div>
-    </div>
+    <section className={clsx("flex justify-center items-center", className)}>
+      <div className="flex flex-row gap-2">
+        <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:.5s]"></div>
+        <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:.3s]"></div>
+        <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:.1s]"></div>
+      </div>
+    </section>
   );
 }
