817408182fdf4e83152d6bb13cebcd4e5149b185
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-24 14:45:47 +0700

    FEAT: add pagination and only show project is isShow and not deleted

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,13 +1,48 @@
 import { PortfolioSchema } from "@/lib/schema/portfolioSchema";
 import { db } from "@/server/db";
+import { Prisma } from "@prisma/client";
 import { z } from "zod";
 
-export const getAllPortfolio = async () => {
-  return await db.portfolio.findMany();
+interface getAllPortfolioProps {
+  page?: number;
+  limit?: number;
+  orderBy?: Prisma.Enumerable<Prisma.PortfolioOrderByWithRelationInput>;
+}
+
+export const getAllPortfolio = async ({
+  page = 1,
+  limit = 10,
+  orderBy,
+}: getAllPortfolioProps) => {
+  const offset = (page - 1) * limit;
+  const data = await db.portfolio.findMany({
+    where: {
+      deletedAt: null,
+      isShow: true,
+    },
+    skip: offset,
+    take: limit,
+    orderBy,
+  });
+  const total = await countTotalProjects();
+  return {
+    data,
+    paging: {
+      page,
+      limit,
+      totalPage: total % limit === 0 ? total / limit : Math.ceil(total / limit),
+    },
+  };
 };
 
 export const getPortfolioById = async (id: string) => {
-  return await db.portfolio.findUnique({ where: { id } });
+  return await db.portfolio.findUnique({
+    where: {
+      id,
+      deletedAt: null,
+      isShow: true,
+    },
+  });
 };
 
 export const createPortfolio = async (
@@ -20,7 +55,14 @@ export const updatePortfolio = async (
   id: string,
   data: z.infer<typeof PortfolioSchema>
 ) => {
-  return await db.portfolio.update({ where: { id }, data });
+  return await db.portfolio.update({
+    where: {
+      id,
+      deletedAt: null,
+      isShow: true,
+    },
+    data,
+  });
 };
 
 export const deletePortfolio = async (id: string) => {
@@ -48,6 +90,15 @@ export const countOngoingProjects = async () => {
   return await db.portfolio.count({
     where: {
       completedAt: null,
+    },
+  });
+};
+
+export const countTotalProjects = async () => {
+  return await db.portfolio.count({
+    where: {
+      deletedAt: null,
+      isShow: true,
     },
   });
 };
