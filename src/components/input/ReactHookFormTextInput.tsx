b9f989ac27ce441279e7779235df534c5ca1d010
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-26 14:31:03 +0700

    FEAT: add form input text

---
+++ src/components/input/ReactHookFormTextInput.tsx
@@ -0,0 +1,52 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+
+interface ReactHookFormTextInputProps<T extends FieldValues> {
+  form: UseFormReturn<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+}
+
+export default function ReactHookFormTextInput<T extends FieldValues>({
+  form,
+  name,
+  label,
+  placeholder,
+}: ReactHookFormTextInputProps<T>) {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder ?? label}
+              value={
+                typeof field.value === "string"
+                  ? field.value
+                  : field.value != null
+                  ? String(field.value)
+                  : ""
+              }
+              onChange={field.onChange}
+              onBlur={field.onBlur}
+              ref={field.ref}
+              name={field.name}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
