1993c147bc80d052f41930976e110c032d4d2c3e
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-14 14:35:20 +0700

    FEAT: create cloudinary for uploading and delete image

---
+++ src/lib/cloudinary/cloudinary.ts
@@ -0,0 +1,50 @@
+import {
+  v2 as cloudinary,
+  UploadApiErrorResponse,
+  UploadApiResponse,
+} from "cloudinary";
+import streamifier from "streamifier";
+
+cloudinary.config({
+  secure: true,
+});
+
+/**
+ * Upload buffer ke Cloudinary
+ * @param buffer Buffer dari file
+ * @returns Promise<UploadApiResponse>
+ */
+const uploadToCloudinary = (buffer: Buffer): Promise<UploadApiResponse> => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: "catalog_uploads" },
+      (
+        error: UploadApiErrorResponse | undefined,
+        result: UploadApiResponse | undefined
+      ) => {
+        if (error || !result) return reject(error);
+        resolve(result);
+      }
+    );
+
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
+
+/**
+ * Hapus file dari Cloudinary berdasarkan publicId
+ * @param publicId ID publik dari file di Cloudinary
+ * @returns Promise<{ result: string }>
+ */
+const deleteFromCloudinary = (
+  publicId: string
+): Promise<{ result: string }> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.destroy(publicId, (error, result) => {
+      if (error) return reject(error);
+      resolve(result as { result: string });
+    });
+  });
+};
+
+export { cloudinary, deleteFromCloudinary, uploadToCloudinary };
