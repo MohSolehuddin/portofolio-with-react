c964989f127b757097ee9f9fdb8859ed3195ac14
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-14 14:35:56 +0700

    FIX: input schema and create response schema

--- src/lib/schema/portfolioSchema.ts
+++ src/lib/schema/portfolioSchema.ts
@@ -15,9 +15,19 @@ export const PortfolioSchema = z.object({
 });
 
 export const PortfolioInputSchema = PortfolioSchema.extend({
+  image: z
+    .instanceof(File)
+    .refine((file) => file instanceof File, {
+      message: "The image must be a valid file.",
+    })
+    .optional(),
+});
+
+export const PortfolioResponseSchema = PortfolioSchema.extend({
   isPrivate: z.coerce.boolean().optional(),
   description: z.string().nullable().optional(),
   deletedAt: z.date().nullable().optional(),
 });
 
 export type PortfolioInputType = z.infer<typeof PortfolioInputSchema>;
+export type PortfolioResponseType = z.infer<typeof PortfolioResponseSchema>;
