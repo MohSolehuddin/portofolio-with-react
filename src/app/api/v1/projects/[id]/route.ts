df2bbde8be5b9875e7afed8599d516faec3fc77c
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-15 16:15:24 +0700

    REAFACTOR: recivet a Multipart form data

--- src/app/api/v1/projects/[id]/route.ts
+++ src/app/api/v1/projects/[id]/route.ts
@@ -1,6 +1,11 @@
 import { createErrorLog } from "@/data/errorLog";
-import { deletePortfolio, updatePortfolio } from "@/data/portfolio";
-import { PortfolioSchema } from "@/lib/schema/portfolioSchema";
+import {
+  deletePortfolio,
+  updatePortfolio,
+  updatePortfolioImage,
+} from "@/data/portfolio";
+import { updateImageInCloudinary } from "@/lib/cloudinary/cloudinary";
+import { PortfolioInputSchema } from "@/lib/schema/portfolioSchema";
 import { db } from "@/server/db";
 
 export async function GET(
@@ -52,14 +57,26 @@ export async function PUT(
   req: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await params;
+  if (!id) return Response.json({ message: "id is required" }, { status: 400 });
+
+  const formData = await req.formData();
+
+  const rawImage = formData.get("image");
+  const data = Object.fromEntries(formData);
+  delete data.id;
+  delete data.image;
+
+  const transformDataToValidateData = {
+    ...data,
+    isPrivate: data.isPrivate === "true",
+    isShow: data.isShow === "true",
+  };
+
   try {
-    const { id } = await params;
-    if (!id)
-      return Response.json({ message: "id is required" }, { status: 400 });
-
-    const data = await req.json();
-
-    const validatedData = PortfolioSchema.safeParse(data);
+    const validatedData = PortfolioInputSchema.safeParse(
+      transformDataToValidateData
+    );
     if (!validatedData.success)
       return Response.json(validatedData.error, { status: 400 });
 
@@ -67,8 +84,22 @@ export async function PUT(
       ...validatedData.data,
     });
 
+    (async () => {
+      if (rawImage && rawImage instanceof File) {
+        const buffer = Buffer.from(await rawImage.arrayBuffer());
+        const uploadedImage = await updateImageInCloudinary(
+          updatedPortfolio.imageId ?? "",
+          buffer
+        );
+        const imageUrl = uploadedImage.secure_url as string;
+        const imageId = uploadedImage.public_id as string;
+        await updatePortfolioImage(id, imageUrl, imageId);
+      }
+    })();
+
     return Response.json({ data: updatedPortfolio }, { status: 200 });
   } catch (error) {
+    console.log({ error });
     const createdError = await createErrorLog(JSON.stringify(error));
     return Response.json(
       { message: "Something went wrong", requestId: createdError?.id },
