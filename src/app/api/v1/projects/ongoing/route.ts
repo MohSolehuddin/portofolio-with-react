d3c03617944dd7db2f976f962bad6fc292a6e500
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-29 09:50:59 +0700

    FEAT: geting data from radis if exist and syncronaise data in background job

--- src/app/api/v1/projects/ongoing/route.ts
+++ src/app/api/v1/projects/ongoing/route.ts
@@ -1,6 +1,34 @@
 import { countOngoingProjects } from "@/data/portfolio";
+import { redisClient } from "@/lib/redis/redisClient";
 
 export async function GET() {
-  const result = await countOngoingProjects();
-  return Response.json({ data: result }, { status: 200 });
+  try {
+    const redisResult = await redisClient().get("ongoingProjects");
+    let result = Number(redisResult);
+
+    if (isNaN(result) || redisResult === null) {
+      result = await countOngoingProjects();
+    }
+
+    if (result !== Number(redisResult)) {
+      await redisClient().set("ongoingProjects", result);
+    }
+
+    countOngoingProjects()
+      .then((count) => {
+        redisClient()
+          .set("ongoingProjects", count)
+          .catch((err) => {
+            console.error("Error updating ongoing projects in Redis:", err);
+          });
+      })
+      .catch((err) => {
+        console.error("Error during background counting:", err);
+      });
+
+    return Response.json({ data: result }, { status: 200 });
+  } catch (err) {
+    console.error("Error during GET request:", err);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
