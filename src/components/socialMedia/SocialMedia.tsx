56f358d50aa0fd50250a17536d4e59908d312ec3
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-29 13:29:40 +0700

    FEAT: optimalize SEO and assebilitas

--- src/components/socialMedia/SocialMedia.tsx
+++ src/components/socialMedia/SocialMedia.tsx
@@ -3,13 +3,22 @@ import { FaGithub, FaInstagram, FaTiktok } from "react-icons/fa";
 export default function SocialMedia() {
   return (
     <section className="social-media flex justify-center items-center gap-6 text-3xl text-navy dark:text-white max-xl:text-xl">
-      <Link href="https://github.com/MohSolehuddin" target="_blank">
+      <Link
+        href="https://github.com/MohSolehuddin"
+        target="_blank"
+        aria-label="Github Moh Solehuddin">
         <FaGithub />
       </Link>
-      <Link href="https://www.tiktok.com/@msytcode" target="_blank">
+      <Link
+        href="https://www.tiktok.com/@msytcode"
+        target="_blank"
+        aria-label="Tiktok Moh Solehuddin">
         <FaTiktok />
       </Link>
-      <Link href="https://www.instagram.com/msytc" target="_blank">
+      <Link
+        href="https://www.instagram.com/msytc"
+        target="_blank"
+        aria-label="Instagram Moh Solehuddin">
         <FaInstagram />
       </Link>
     </section>
